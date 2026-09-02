3839ca7a7f87c91040e10ccbc45388bc82eb4545
Krish-Om <krishombasukala@gmail.com>
2026-09-02 12:30:20 +0545

    feat : finished setting up the contact message route, services, and controller.

--- backend/src/modules/contact/contacts.routes.ts
+++ backend/src/modules/contact/contacts.routes.ts
@@ -1,14 +1,9 @@
-import Router,{type Request, type Response} from "express";
-import contactController from "./contacts.controller.ts";
+import express from "express";
+import contactsController from "./contacts.controller.ts";
 
-const contacts = Router();
-contacts.get("/", async (req: Request, res: Response) => {
-    req.log.info("GET /api/v1/contacts endpoint hit");
-    await contactController.getAllContacts(req, res);
-});
-contacts.post("/", async (req: Request, res: Response) => {
-    req.log.info("POST /api/v1/contacts endpoint hit with body: " + JSON.stringify(req.body));
-    await contactController.createContact(req, res);
-});
+const contacts = express.Router();
+
+contacts.post("/", contactsController.createContact);
+contacts.get("/", contactsController.getContacts);
 
 export default contacts;
